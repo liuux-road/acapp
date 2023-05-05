b851146bc207c7f51770525f0cc03a55a574d289
liuux <zuioujia@qq.com>
2023-05-05 15:10:32 +0800

    (4)first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("game/view.py 下写的一个index函数收到request后得到的一个反馈")
+# index函数得到一个路由，返回一个字符串
+
